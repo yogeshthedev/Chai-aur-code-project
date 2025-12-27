c0107f6268bbc96463114fedef47f4b458ae206a
Yogesh Meena <developeryogeshh@gmail.com>
2025-12-27 15:20:12 +0530

    commit

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,20 +1,63 @@
-import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+  const ChannelId = req.user._id;
+
+  const totalVideos = await Video.countDocuments({ owner: ChannelId });
+
+  const totalSubscribers = await Subscription.countDocuments({
+    channel: ChannelId,
+  });
+
+  const myVideos = await Video.find({ owner: req.user._id }, { _id: 1 });
+
+  const videoIds = myVideos.map((video) => video._id);
+
+  const totalLikes = await Like.countDocuments({
+    video: { $in: videoIds },
+  });
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        totalVideos,
+        totalSubscribers,
+        totalLikes,
+      },
+      "Channel stats fetched successfully"
+    )
+  );
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-})
+  // TODO: Get all the videos uploaded by the channel
 
-export {
-    getChannelStats, 
-    getChannelVideos
-    }
+  const ChannelId = req.user._id;
+
+  const channelVideos = await Video.find({ owner: ChannelId }).sort({
+    createdAt: -1,
+  });
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        channelVideos,
+        channelVideos.length === 0
+          ? "Video not available"
+          : "Videos feteched succesfully"
+      )
+    );
+});
+
+export { getChannelStats, getChannelVideos };
